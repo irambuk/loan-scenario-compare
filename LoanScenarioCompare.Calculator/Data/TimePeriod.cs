60b07acbea1bf442204c7861f6eeab42cf2be3d5
Indrajith Rambukkanage <indrajith_rp@hotmail.com>
2017-04-21 14:17:45 +1000

    Basic loan repayment calculations working

---
+++ LoanScenarioCompare.Calculator/Data/TimePeriod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanScenarioCompare.Calculator.Data
+{
+    public class TimePeriod
+    {
+        public TimePeriodTypes TimePeriodType { get; set; }
+
+        public int Count { get; set; }
+    }
+}
