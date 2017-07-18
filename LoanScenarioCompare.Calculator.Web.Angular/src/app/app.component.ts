e4f1a9557fc8516967c0d3cbf79c7dad2647af4b
Indrajith Rambukkanage <pushpaka.rambukkanage@gmail.com>
2017-07-19 05:43:26 +1000

    Removing the angular project to recreate later

--- LoanScenarioCompare.Calculator.Web.Angular/src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-  selector: 'loan-compare-app',
-  template: `<loan-calculator></loan-calculator>`,
-})
-export class AppComponent  { name = 'Angular'; }
--- LoanScenarioCompare.Calculator.Web.Angular/src/app/loancalculator.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component, Input } from '@angular/core';
-
-@Component({
-    selector: 'loan-calculator',
-  templateUrl: './loancalculator.component.html',
-  styleUrls: ['./loancalculator.component.css']
-})
-export class LoanCalculatorComponent {  
-}
